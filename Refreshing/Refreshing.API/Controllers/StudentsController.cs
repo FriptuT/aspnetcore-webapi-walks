5e536270314aef85a8ad3e0df3dc7f17e25839c8
FriptuT <friptut@gmail.com>
2024-06-04 20:04:58 +0300

    starting fresh

---
+++ Refreshing/Refreshing.API/Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Refreshing.API.Controllers
+{
+    
+    // https://localhost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET: https://localhost:portnumber/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentsNames = new string[] { "Jonh", "Jane", "Mark", "Emily", "David" };
+
+            return Ok(studentsNames);
+        }
+    }
+}
